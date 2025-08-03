30a0a9dc70d79df3a742b7d6e635f11038865eed
George Kamande <161124335+g3org3zx@users.noreply.github.com>
2025-08-03 19:13:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
             // Create remove button
             const removeBtn = document.createElement('button');
             removeBtn.textContent = 'Remove';
-            removeBtn.className = 'remove-btn';
+            removeBtn.classList.add('remove-btn');
 
             // Assign onclick event to remove button
             removeBtn.onclick = () => {
@@ -57,7 +57,7 @@ document.addEventListener('DOMContentLoaded', () => {
             // Create remove button
             const removeBtn = document.createElement('button');
             removeBtn.textContent = 'Remove';
-            removeBtn.className = 'remove-btn';
+            removeBtn.classList.add('remove-btn');
 
             // Assign onclick event to remove button
             removeBtn.onclick = () => {
